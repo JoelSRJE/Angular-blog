fd7f97220f3b008ad3c6f41fb6b3fe7e11bf0eb5
JoelSRJE <JoelSRJE@hotmail.com>
2023-11-21 17:56:50 +0100

    more setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { AboutComponent } from './views/about/about.component';
+import { CreatePostComponent } from './components/create-post/create-post.component';
+import { ViewPostComponent } from './components/view-post/view-post.component';
+import { FormsModule } from '@angular/forms';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'Contact', component: ContactComponent },
+  { path: 'About', component: AboutComponent },
+  { path: 'Create', component: CreatePostComponent },
+  { path: 'View/:id', component: ViewPostComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), FormsModule],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-blog';
 }
+
+/* NPMs som används 
+
+tsparticles-engine
+tsparticles-slim
+ngx-toastr
+ng-particles
+angular-feather
+
+*/
