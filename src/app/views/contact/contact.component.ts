beaa2d75a71185368ddc4508eaae4eb3504014f2
JoelSRJE <JoelSRJE@hotmail.com>
2023-11-22 15:40:09 +0100

    contact view

--- src/app/views/contact/contact.component.ts
+++ src/app/views/contact/contact.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+interface FormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  subject: string;
+  comment: string;
+}
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
+  formData: FormData = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    subject: '',
+    comment: '',
+  };
 
+  formItems: FormData[] = [];
+
+  submitToConsole() {
+    console.log('Form Data:', this.formData);
+    this.formItems.push({ ...this.formData });
+    this.clearForm();
+  }
+
+  clearForm() {
+    this.formData = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      subject: '',
+      comment: '',
+    };
+  }
+
+  constructor() {}
 }
